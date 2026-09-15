7443ad34a405511ce1a3b6e09baddd087e423461
vedashri182008 <deshmukhvedashri@gmail.com>
2026-09-16 00:11:39 +0530

    Implement Account and Auditor classes

---
+++ Program_13/program13.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+ 
+class Account { 
+private: 
+double balance; 
+ 
+friend class Auditor; 
+ 
+public: 
+explicit Account(double initialBalance) : balance(initialBalance) {} 
+}; 
+ 
+class Auditor { 
+public: 
+void inspect(const Account& account) const { 
+std::cout << "Account Balance: " << account.balance << '\n'; 
+} 
+}; 
+ 
+int main() { 
+Account account(5000.0); 
+Auditor auditor; 
+ 
+auditor.inspect(account); 
+return 0; 
+}
