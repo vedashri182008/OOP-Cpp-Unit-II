7a4d4031fd11c27b48403f48b3fd0909e29978bf
vedashri182008 <deshmukhvedashri@gmail.com>
2026-09-15 23:50:30 +0530

    Implement Vehicle hierarchy with Car and Boat classes

---
+++ Program_10/program10.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class Vehicle {
+public:
+virtual void move() const {
+std::cout << "Vehicle is moving\n";
+}
+virtual ~Vehicle() = default;
+};
+
+class Car : public Vehicle {
+public:
+void move() const override {
+std::cout << "Car moves on roads\n";
+}
+};
+class Boat : public Vehicle {
+public:
+void move() const override {
+std::cout << "Boat moves on water\n";
+}
+};
+int main() {
+Car car;
+Boat boat;
+
+car.move();
+boat.move();
+return 0;
+}
