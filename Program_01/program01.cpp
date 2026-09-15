f8820bdb98a616fb7241c44c66b34c0b0cde5cbd
vedashri182008 <deshmukhvedashri@gmail.com>
2026-09-15 23:11:41 +0530

    Implement Person and Student classes with display methods

--- Program_01/program01.cpp
+++ Program_01/program01.cpp
@@ -1,1 +1,37 @@
-
+#include <iostream> 
+#include <string> 
+#include <utility> 
+using namespace std;
+ 
+class Person { 
+protected: 
+string name; 
+ 
+public: 
+explicit Person(string personName) : name(move(personName)) {} 
+ 
+void displayName() const { 
+cout << "Name: " << name << '\n'; 
+} 
+}; 
+ 
+class Student : public Person { 
+private: 
+ 
+int rollNumber; 
+ 
+public: 
+Student(string studentName, int roll) 
+: Person(move(studentName)), rollNumber(roll) {} 
+ 
+void displayStudent() const { 
+displayName(); 
+cout << "Roll Number: " << rollNumber << '\n'; 
+} 
+}; 
+ 
+int main() { 
+Student student("Amit", 101); 
+student.displayStudent(); 
+return 0; 
+}
