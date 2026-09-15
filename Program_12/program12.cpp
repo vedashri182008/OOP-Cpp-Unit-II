337121dcacb44d8810df20ff85031579369ba1d8
vedashri182008 <deshmukhvedashri@gmail.com>
2026-09-16 00:08:52 +0530

    Implement Person hierarchy with TeachingAssistant class

---
+++ Program_12/program12.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+#include <string> 
+#include <utility> 
+ 
+class Person { 
+protected: 
+std::string name; 
+ 
+public: 
+explicit Person(std::string personName) : name(std::move(personName)) {} 
+ 
+void displayName() const { 
+std::cout << "Name: " << name << '\n'; 
+} 
+}; 
+ 
+class Student : virtual public Person { 
+public: 
+Student() : Person("Unknown") {} 
+}; 
+class Employee : virtual public Person { 
+public: 
+Employee() : Person("Unknown") {} 
+}; 
+ 
+class TeachingAssistant : public Student, public Employee { 
+public: 
+explicit TeachingAssistant(std::string assistantName) 
+: Person(std::move(assistantName)), Student(), Employee() {} 
+}; 
+ 
+int main() { 
+TeachingAssistant assistant("Riya"); 
+assistant.displayName(); 
+return 0; 
+}
